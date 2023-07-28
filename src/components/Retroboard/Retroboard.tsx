ba4ece3210f98b2d51454d762cf0fa4059e44f1f
Kevin Browne <kbrowne97@gmail.com>
2023-07-28 05:23:36 -0400

    installs uuid, adds createItem function

--- src/components/Retroboard/Retroboard.tsx
+++ src/components/Retroboard/Retroboard.tsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-type itemInfoType = {
+type itemType = {
+  id: string;
   name: string;
   category: string;
 };
 
-type itemType = null | {
-  id: string;
-  info: itemInfoType;
-};
+const id = uuid();
 
 const Retroboard = (): React.ReactNode => {
   const [items, setItems] = useState<itemType[]>([
-    { id: "test", info: { name: "First task", category: "went well" } },
+    { id: "test", name: "first task", category: "all done" },
   ]);
 
-  // setItems([
-  //   { id: "test", info: { name: "First task", category: "went well" } },
-  // ]);
+  const createItem = (itemInfo: itemType) => {
+    const id = uuid();
+    const { name, category } = itemInfo;
+    // build new item obj and add to state
+    setItems([...items, { name, category, id }]);
+  };
 
   return (
     <div className="retro-board">
